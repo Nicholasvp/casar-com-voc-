07017b35f36d56ccf6eb08a5f0140b7005297a76
nicholas <nicholasvpinheiro@gmail.com>
2024-01-26 17:27:19 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    document.getElementById("whatsapp-btn").addEventListener("click", function() {
+      // Abre o link do Whatsapp
+      window.open("https://api.whatsapp.com/send?phone=84991791015", "_blank");
+    });
+  
+    // Adiciona um evento de clique ao botão do Instagram
+    document.getElementById("instagram-btn").addEventListener("click", function() {
+      // Abre o link do Instagram
+      window.open("https://www.instagram.com/casarcomvoce_/", "_blank");
+    });
+  });
